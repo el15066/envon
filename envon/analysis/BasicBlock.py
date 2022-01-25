el15066/envon

commit ac45eb4cff0cdce750056b40dfa93cc3bc737eb0

BasicBlock: implement out_edges() and remove_edge()

envon/analysis/BasicBlock.py
@@ -61,6 +61,13 @@ class BasicBlock:
     def jump_edges(self):
         return iter(self._jump_edges) # TODO: maybe frozenset?
 
+    def out_edges(self):
+        res = []
+        b2  = self._fallthrough_edge
+        if  b2 is not None:         res.append(b2)
+        for b2 in self._jump_edges: res.append(b2)
+        return res
+
     def accept_edge(self, src):
         assert not src.skip
         assert src not in self._in_edges
@@ -101,6 +108,12 @@ class BasicBlock:
             self._fallthrough_edge = None
             b.forget_edge(self)
         return b
+
+    def remove_edge(self, b):
+        if b == self._fallthrough_edge:
+            self.remove_fallthrough_edge()
+        else:
+            self.remove_jump_edge(b)
 
     def get_mem(self):
         n = self._mem

commit 396bfbcfa436594627aae1bbcd30ec8087b2598e

BasicBlock: fix missing MemPhis
Was broken by ac45eb4

envon/analysis/BasicBlock.py
@@ -43,13 +43,16 @@ class BasicBlock:
     def phis(self):
         return iter(self._phis)
 
+    def _add_phi(self, phi):
+        self._phis.append(phi)
+        self.ns.appendleft(phi)
+
     def create_stack_phi(self, sp):
         if self.offset == 0: return DummyInstruction(self)
         else:
             if sp >= -90: phi = StackPhi(           self, sp)
             else:         phi = StackPhiLoopBreaker(self, sp) # arbitrarily stop stack dig loops
-            self._phis.append(phi)
-            self.ns.appendleft(phi)
+            self._add_phi(phi)
             return phi
 
     def in_edges(self):
@@ -119,6 +122,7 @@ class BasicBlock:
         n = self._mem
         if n is None:
             n = MemPhi(self)
+            self._add_phi(n)
             self._memphi = n
             self._mem    = n
         return n